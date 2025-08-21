894a07954b083267cf582ffb1de260495ed61891
LYJ0304 <omlmg0304@naver.com>
2025-08-22 00:05:02 +0900

    feat: /api/receipts 엔드포인트 구현 (multipart 업로드)

--- src/main/java/com/kumdoriGrow/backend/api/receipt/ReceiptController.java
+++ src/main/java/com/kumdoriGrow/backend/api/receipt/ReceiptController.java
@@ -1,4 +1,24 @@
-package com.kumdoriGrow.backend.api.receipt.dto;
+package com.kumdoriGrow.backend.api.receipt;
 
+import com.kumdoriGrow.backend.api.receipt.dto.ReceiptResponse;
+import com.kumdoriGrow.backend.domain.receipt.ReceiptService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/receipts")
+@RequiredArgsConstructor
 public class ReceiptController {
+
+    private final ReceiptService receiptService;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ReceiptResponse parse(@RequestPart("file") MultipartFile file) {
+        return receiptService.process(file);
+    }
 }
