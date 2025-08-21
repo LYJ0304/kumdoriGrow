e1295a8470baedbda6bbe1f7d53f845d92c642a6
LYJ0304 <omlmg0304@naver.com>
2025-08-21 13:35:16 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/kumdoriGrow/backend/kumdoriGrowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kumdoriGrow.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class kumdoriGrowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(kumdoriGrowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kumdoriGrow/backend/KumdoriGrowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumdoriGrow.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KumdoriGrowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
