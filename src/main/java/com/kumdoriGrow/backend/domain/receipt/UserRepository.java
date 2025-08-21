3c1cb575c61c4f11ea787d3e8383e932d4bd38ac
LYJ0304 <omlmg0304@naver.com>
2025-08-22 04:32:19 +0900

    feat: UserRepository 추가

--- src/main/java/com/kumdoriGrow/backend/domain/receipt/UserRepository.java
+++ src/main/java/com/kumdoriGrow/backend/domain/receipt/UserRepository.java
@@ -1,4 +1,7 @@
 package com.kumdoriGrow.backend.domain.receipt;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.kumdoriGrow.backend.domain.user.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
