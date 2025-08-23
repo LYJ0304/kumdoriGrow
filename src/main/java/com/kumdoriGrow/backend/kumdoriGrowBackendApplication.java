30addcb464d5f5d18c9b8ad48f39e0a0db2544f8
LYJ0304 <omlmg0304@naver.com>
2025-08-24 02:53:25 +0900

    fix: 오류 관견 수정

--- src/main/java/com/kumdoriGrow/backend/kumdoriGrowBackendApplication.java
+++ src/main/java/com/kumdoriGrow/backend/kumdoriGrowBackendApplication.java
@@ -1,12 +1,13 @@
 package com.kumdoriGrow.backend;
 
 import com.kumdoriGrow.backend.config.OcrProperties;
+import com.kumdoriGrow.backend.config.XpProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(scanBasePackages = "com.kumdoriGrow.backend")
-@EnableConfigurationProperties(OcrProperties.class)
+@EnableConfigurationProperties({OcrProperties.class, XpProperties.class})
 public class kumdoriGrowBackendApplication {
 
 	public static void main(String[] args) {
